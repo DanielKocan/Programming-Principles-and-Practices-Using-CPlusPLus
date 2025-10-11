8fb4293da018dfb6f845143dd05dfbcf57292295
DanielKocan <danuk2004@gmail.com>
2025-10-11 13:41:09 +0200

    [ADD] (3), done with chapter 2

--- TryingMyBest2ObjectsTypesAndValues/TryingMyBest2ObjectsTypesAndValues.cpp
+++ TryingMyBest2ObjectsTypesAndValues/TryingMyBest2ObjectsTypesAndValues.cpp
@@ -2,6 +2,19 @@
 
 using namespace std; // This is a directive that tells the compiler to use the standard namespace.
 
+// A TYPE defines a set of possible values and set of operation (for an object)
+// An OBJECT is some memory that holds a value of a specific (given) type.
+// a VALUE is a set of bits in memory intrerpreted according to a specific type.
+// a VARIABLE is a named object
+// a DECLARATION is a statement that gives a name and a type to an object.
+// a DEFINITION is a declaration that sets aside memory for an object.
+
+// For historically and pracital reasons C++ offers four notation for initialization:
+// T a = b;
+// T a{b}; 
+// T a = {b};
+// T a(b); 
+
 int main()
 {
     cout << "Please enter your name \n";
@@ -18,7 +31,18 @@ int main()
     cout << "Test name is:" << name << endl;
 	cout << "Please enter your first name and age \n";
 	string first_name = "???";
-	int age = -1;
+	int age = -1; // a name for the variable must start with a letter, can be letter, digits or underscore, no spaces, no special character.
 	cin >> first_name >> age;
 	cout << "Hello, " << first_name << " (age " << age << ")\n";
+
+	double d = 0;
+	while (cin >> d)
+	{
+		int i = d;
+		char c = i;
+		cout << "d==" << d
+			<< " i==" << i
+			<< " c==" << c
+			<< " char (" << c << ")\n";
+	}
 }
